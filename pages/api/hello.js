2010770e09e55fe9824e0c66aa20f1e115cfa2ff
vsamaru <vsamaru@gmail.com>
2021-09-22 04:12:11 +0300

    commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { c } from '../../clo'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+	var v = 0
+
+    try {
+       v = await c("https://api.telegram.org/file/bot976117495:AAEUmJdnuryTxuX6VETU_rrIq-1nDi6W1lU/photos/file_9.jpg")
+    } catch (err) {
+        v = err.stack
+    }
+
+  res.status(200).json({ name: v })
 }
